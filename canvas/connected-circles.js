01908812f5b758fda72b55b1f313c703c0d60cd5
Felipe Reis <felipe.gdr@gmail.com>
2020-03-17 19:16:56 +1100

    Canvas: Dynamic circle placement
    based on number of circles and total circumference of composition

--- canvas/connected-circles.js
+++ canvas/connected-circles.js
@@ -2,32 +2,41 @@ const draw = () => {
     const canvas = document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
     const canvasCenter = [canvas.offsetHeight / 2, canvas.offsetWidth / 2];
-    const defaultRadius = 25
+    const defaultRadius = 20;
     const circleAngle = toRadians(360);
-    const distance = 50;
+    const distance = 70;
+    const distanceAdjustment = 20;
+    const overlapSecurityMargin = 50;
 
-    ctx.beginPath();
-
-    const circle1 = {
+    const mainCircle = {
         center: canvasCenter,
-        radius: defaultRadius,
+        radius: 40,
+        text: 'Center',
+        backgroundColor: 'rgba(255, 0, 0, .5)',
     };
 
-    drawCircle(circle1);
+    const bigRadius = mainCircle.radius + distance + defaultRadius;
+    const bigCircumference = 2 * Math.PI * bigRadius;
+    const maxNumberOfCirclesWithoutOverlap = Math.floor((bigCircumference - overlapSecurityMargin) / (defaultRadius * 2));
 
-    for (let x = 0; x < 4; x++) {
-        const angle = Math.floor(Math.random() * Math.floor(360));
-        const newCircle = fromCircle(circle1, toRadians(angle), distance, defaultRadius);
-        drawCircle(newCircle);
-        connectCircles(circle1, newCircle);
+    console.log('main circumference: ' + bigRadius, bigCircumference, maxNumberOfCirclesWithoutOverlap);
+
+    drawCircle(mainCircle);
+
+    writeCircles(11);
+
+    function writeCircles(count) {
+        for (let x = 1; x <= count; x++) {
+            const thisDistance = count >= maxNumberOfCirclesWithoutOverlap ?
+                x % 2 === 0 ? distance + distanceAdjustment : distance - distanceAdjustment :
+                distance;
+
+            const angle = 360 / count * x;
+            const newCircle = fromCircle(mainCircle, toRadians(angle), thisDistance, defaultRadius);
+            connectCircles(mainCircle, newCircle);
+            drawCircle(newCircle);
+        }
     }
-
-    // drawPoint(getCoordinates(canvasCenter, defaultRadius, toRadians(0)));
-    // drawPoint(getCoordinates(canvasCenter, defaultRadius, toRadians(90)));
-    // drawPoint(getCoordinates(canvasCenter, defaultRadius, toRadians(180)));
-    // drawPoint(getCoordinates(canvasCenter, defaultRadius, toRadians(270)));
-
-    ctx.stroke();
 
     function fromCircle({ center, radius }, angle, distance, newRadius) {
         const newCenter = getCoordinates(center, radius + newRadius + distance, angle);
@@ -35,14 +44,18 @@ const draw = () => {
         return {
             center: newCenter,
             radius: newRadius,
+            backgroundColor: 'rgba(0, 0, 255, .5)',
         };
     }
 
-    function drawCircle({ center, radius }) {
+    function drawCircle({ center, radius, backgroundColor }) {
         const [centerX, centerY] = center;
+        ctx.beginPath();
         ctx.moveTo(centerX + radius, centerY)
         ctx.arc(centerX, centerY, radius, 0, circleAngle);
+        ctx.fillStyle = backgroundColor;
         ctx.fill();
+        ctx.stroke();
     }
 
     function getCoordinates([centerX, centerY], distance, angle) {
@@ -55,12 +68,25 @@ const draw = () => {
 
     function drawPoint([centerX, centerY]) {
         const radius = 2;
+        ctx.beginPath();
         ctx.moveTo(centerX + radius, centerY);
         ctx.arc(centerX, centerY, radius, 0, circleAngle);
+        ctx.stroke();
     }
 
     function connectCircles(circle1, circle2) {
-        ctx.moveTo(...circle1.center);
-        ctx.lineTo(...circle2.center);
+        const angle1 = Math.atan2(circle1.center[1] - circle2.center[1], circle1.center[0] - circle2.center[0]);
+        const angle2 = Math.atan2(circle2.center[1] - circle1.center[1], circle2.center[0] - circle1.center[0]);
+
+        const touch1 = getCoordinates(circle2.center, circle2.radius, angle1);
+        const touch2 = getCoordinates(circle1.center, circle1.radius, angle2);
+
+        drawPoint(touch1);
+        drawPoint(touch2);
+
+        ctx.beginPath();
+        ctx.moveTo(...touch1);
+        ctx.lineTo(...touch2);
+        ctx.stroke();
     }
 }
