f966e33c6ef3690503a751aec828732d9e7187a2
Felipe Reis <felipe.gdr@gmail.com>
2020-12-24 15:27:27 +1100

    layered slices

--- d3/nightingale-chart/src/App.js
+++ d3/nightingale-chart/src/App.js
@@ -1,10 +1,41 @@
+import { max } from "d3";
 import "./App.css";
 import { Chart } from "./nightingale-chart";
+
+const randomInt = max => Math.floor(Math.random() * (max + 1));
+
+const highestScore = 4;
+const areasCount = 12;
+const subAreasMax = 6;
+
+const pascalData2 = [
+  [
+    [4,4,4,4],
+    [4,4],
+    [4,4,4],
+  ]
+];
+
+const pascalData = () => {
+  const result = [];
+
+  for(let x = 0; x < areasCount; x++) {
+    const subAreas = [];
+
+    for(let y = 0; y < randomInt(subAreasMax); y++) {
+      subAreas.push(randomInt(highestScore));
+    }
+
+    result.push(subAreas);
+  }
+
+  return result;
+};
 
 function App() {
   return (
     <div className="App">
-      <Chart n={24} m={12} />
+      <Chart data={pascalData()} config={{ highestScore }}/>
     </div>
   );
 }
--- d3/nightingale-chart/src/nightingale-chart/index.js
+++ d3/nightingale-chart/src/nightingale-chart/index.js
@@ -3,36 +3,43 @@ import * as d3 from "d3";
 
 const arcFn = d3
   .arc()
-  .innerRadius(({ innerRadius}) => innerRadius)
-  .outerRadius(({ outerRadius}) => outerRadius)
+  .innerRadius(({ innerRadius }) => innerRadius)
+  .outerRadius(({ outerRadius }) => outerRadius)
   .startAngle(({ startAngle }) => startAngle)
   .endAngle(({ endAngle }) => endAngle);
 
 const totalInnerRadius = 50;
 const totalOuterRadius = 300;
 
-export const Chart = ({ n, m }) => {
-  const radiusSize = (totalOuterRadius - totalInnerRadius) / m;
+export const Chart = ({ data, config: { highestScore } }) => {
+  const slices = data.length;
 
   return (
     <svg viewBox="-320 -320 640 640" style={{ maxWidth: 640 }}>
-      {Array.from({ length: n }, (_, i) => {
-          const startAngle = (i / n) * 2 * Math.PI;
-          const endAngle = ((i + 1) / n) * 2 * Math.PI;
-          return (
-              <Fragment>
-                  {Array.from({ length: m}, (_, j) => {
-                    const innerRadius = (radiusSize * j) + totalInnerRadius;
-                    const outerRadius = innerRadius + radiusSize;
+      {Array.from({ length: slices }, (_, i) => {
+        const startAngle = (i / slices) * 2 * Math.PI;
+        const endAngle = ((i + 1) / slices) * 2 * Math.PI;
+        const maxDepth = data[i].length * highestScore;
+        let currentLevel = 0;
+        return (
+          <Fragment key={i}>
+            {data[i].map((value, j) => {
+              const radiusSize = (totalOuterRadius - totalInnerRadius) / maxDepth;
+              const innerRadius = radiusSize * currentLevel + totalInnerRadius;
+              const outerRadius = innerRadius + (radiusSize * value);
+              currentLevel += value;
 
-                    return <path
-                      stroke="white"
-                      fill={d3.interpolateRainbow(i / n)}
-                      d={arcFn({ startAngle, endAngle, innerRadius, outerRadius})}
-                  ></path>
-                  })}
-              </Fragment>
-          );
+              return (
+                <path
+                  key={j}
+                  stroke="white"
+                  fill={d3.interpolateRainbow(i / slices)}
+                  d={arcFn({ startAngle, endAngle, innerRadius, outerRadius })}
+                ></path>
+              );
+            })}
+          </Fragment>
+        );
       })}
     </svg>
   );
